ae0a102f64c0d6882121d0d478e39ca55b9c92a5
mikemuoio <michael1muoio@gmail.com>
2016-08-30 13:45:12 -0400

    Delete Assignment1.m

--- Assignment1.m
+++
@@ -1,5 +0,0 @@
-%%deleted the date column and only imported the Spot Price data
-%%saved the file as 'NYH_GasPrices_86-16.xls'
-[PRICE] = xlsread('NYH_GasPrices_86-16.xls');
-average = mean(PRICE);
-xlswrite('monthly_average_price.xlsx',average);
